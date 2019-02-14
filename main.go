38a4a64ee311d9f0c1daf40d1694f5bffd6f8f9e
Alexander van Trijffel <lexmail@gmail.com>
2019-02-14 13:20:18 +0100

    Change to ssml

--- main.go
+++ main.go
@@ -25,16 +25,21 @@ func main() {
 	// Perform the text-to-speech request on the text input with the selected
 	// voice parameters and audio file type.
 	req := texttospeechpb.SynthesizeSpeechRequest{
-		// Set the text input to be synthesized.
 		Input: &texttospeechpb.SynthesisInput{
-			InputSource: &texttospeechpb.SynthesisInput_Text{Text: string(content)},
+			InputSource: &texttospeechpb.SynthesisInput_Ssml{Ssml: string(content)},
 		},
+
+		// Set the text input to be synthesized.
+		// Input: &texttospeechpb.SynthesisInput{
+		// 	InputSource: &texttospeechpb.SynthesisInput_Text{Text: string(content)},
+		// },
+
 		// Build the voice request, select the language code ("en-US") and the SSML
 		// voice gender ("neutral").
 		Voice: &texttospeechpb.VoiceSelectionParams{
 			LanguageCode: "en-US",
 			Name:         "en-US-Wavenet-D",
-			//SsmlGender:   texttospeechpb.SsmlVoiceGender_NEUTRAL,
+			// Woman voice: en-US-Wavenet-C
 		},
 		// Select the type of audio file you want returned.
 		AudioConfig: &texttospeechpb.AudioConfig{
